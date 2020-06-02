337175878e912a12dd481fe255b605a5cd0098f8
daerhongge <1294719673@qq.com>
2020-06-02 16:16:28 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/ghl/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghl.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-order/src/test/java/com/ghl/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghl.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
